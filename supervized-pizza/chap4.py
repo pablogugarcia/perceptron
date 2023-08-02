003cc5de5a1761165c85209baa2a405ed2fe10ce
pablogugarcia <pablo.garcia366@gmail.com>
2023-08-02 11:17:21 -0300

    feat: upgrading with multiple dimensions

--- supervized-pizza/chap4.py
+++ supervized-pizza/chap4.py
@@ -13,4 +13,18 @@ Y = y.reshape(-1, 1)
 # we initialize w to a matrix of zeros, the number of columns in the input matrix and 1 column for each weight value
 w = np.zeros((X.shape[1], 1))
 
+
+def predict(X, w):
+    return np.matmul(X, w)
+
+
+def loss(X, Y, w):
+    return np.average((predict(X, w) - Y) ** 2)
+
+
+def gradient(X, Y, w):
+    x_transposed = X.T
+    return 2 * np.matmul(x_transposed, (predict(X, w) - Y)) / X.shape[0]
+
+
 print(X.shape)
