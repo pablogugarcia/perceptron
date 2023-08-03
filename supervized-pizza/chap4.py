52fc1024018e2aafa294074fc5af200cb91babc7
pablogugarcia <pablo.garcia366@gmail.com>
2023-08-03 12:59:56 -0300

    feat: wrap all

--- supervized-pizza/chap4.py
+++ supervized-pizza/chap4.py
@@ -2,17 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-x1, x2, x3, y = np.loadtxt("pizza_3_vars.txt", skiprows=1, unpack=True)
-
-print(x1.shape)
-
-X = np.column_stack((x1, x2, x3))
-# y is in a one dimensional form (30,) instead (30,1), and mixing arrays with matrix could result in unexpected behavior
-Y = y.reshape(-1, 1)
-
-# we initialize w to a matrix of zeros, the number of columns in the input matrix and 1 column for each weight value
-w = np.zeros((X.shape[1], 1))
-
 
 def predict(X, w):
     return np.matmul(X, w)
@@ -23,8 +12,19 @@ def loss(X, Y, w):
 
 
 def gradient(X, Y, w):
-    x_transposed = X.T
-    return 2 * np.matmul(x_transposed, (predict(X, w) - Y)) / X.shape[0]
+    return 2 * np.matmul(X.T, (predict(X, w) - Y)) / X.shape[0]
 
 
-print(X.shape)
+def train(X, Y, iterations, lr):
+    w = np.zeros((X.shape[1], 1))
+    for i in range(iterations):
+        print("Iteration %4d => Loss: %.20f" % (i, loss(X, Y, w)))
+        w -= gradient(X, Y, w) * lr
+    return w
+
+
+x1, x2, x3, y = np.loadtxt("pizza_3_vars.txt", skiprows=1, unpack=True)
+X = np.column_stack((x1, x2, x3))
+Y = y.reshape(-1, 1)
+
+w = train(X, Y, iterations=100000, lr=0.001)
