6bda3dca0a2344f596c9685444ab48a8c53bcfbf
pablogugarcia <pablo.garcia366@gmail.com>
2023-08-09 11:41:07 -0300

    feat: add bias

--- supervized-pizza/chap4.py
+++ supervized-pizza/chap4.py
@@ -24,7 +24,17 @@ def train(X, Y, iterations, lr):
 
 
 x1, x2, x3, y = np.loadtxt("pizza_3_vars.txt", skiprows=1, unpack=True)
-X = np.column_stack((x1, x2, x3))
+# the formula with bias is  	ŷ = x1 * w1 + x2 * w2 + x3 * w3 + b
+# at the end is the same as 	ŷ = x1 * w1 + x2 * w2 + x3 * w3 + x0 * b; when x0 = 1
+# before X = np.column_stack((x1, x2, x3))
+# now we add a new column of ones: np.ones(x1.size)
+X = np.column_stack((np.ones(x1.size), x1, x2, x3))
 Y = y.reshape(-1, 1)
 
 w = train(X, Y, iterations=100000, lr=0.001)
+
+print("\nweights: %s" % w.T)
+print("\nPredictions:")
+
+for i in range(5):
+    print("X[%d] -> %.4f (label: %d) " % (i, predict(X[i], w), Y[i]))
