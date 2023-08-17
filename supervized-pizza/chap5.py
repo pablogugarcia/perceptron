125151795561e89d5607708f43bb141af55cb860
pablogugarcia <pablo.garcia366@gmail.com>
2023-08-17 11:31:44 -0300

    feat: testing classifier algo

--- supervized-pizza/chap5.py
+++ supervized-pizza/chap5.py
@@ -33,3 +33,32 @@ def log_loss(X, Y, w):
 
 def gradient(X, Y, w):
     return np.matmul(X.T, (forward(X, w) - Y)) / X.shape[0]
+
+
+def train(X, Y, iterations, lr):
+    w = np.zeros((X.shape[1], 1))
+    for i in range(iterations):
+        print("Iteration %4d => Loss: %.20f" % (i, log_loss(X, Y, w)))
+        w -= gradient(X, Y, w) * lr
+    return w
+
+
+def test(X, Y, w):
+    total_examples = X.shape[0]
+    correct_results = np.sum(classify(X, w) == Y)
+    success_percent = correct_results * 100 / total_examples
+    print(
+        "\nSuccess: %d/%d (%.2f%%)" % (correct_results, total_examples, success_percent)
+    )
+
+
+# Prepare data
+x1, x2, x3, y = np.loadtxt("police.txt", skiprows=1, unpack=True)
+X = np.column_stack((np.ones(x1.size), x1, x2, x3))
+Y = y.reshape(-1, 1)
+w = train(X, Y, iterations=10000, lr=0.001)
+
+# Test it
+test(X, Y, w)
+
+print("\n Weights: ", w)
